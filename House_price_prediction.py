f932e29a5fc02661147ce241c7a1f87ab9f996c3
Anna1122004 <mathewt9747592371@gmail.com>
2026-09-17 14:18:46 +0530

    Update house price prediction logic and inputs

--- House_price_prediction.py
+++ House_price_prediction.py
@@ -1,42 +1,51 @@
 import streamlit as st
 import joblib
+
 model = joblib.load("house_price_prediction.pkl")
+
 st.title("House Price Prediction")
+
 Area = st.number_input(
     "Enter Area",
     min_value=0.0,
-    value=400.0
+    value=1200.0
 )
+
 Bedrooms = st.number_input(
     "Enter number of bedrooms",
     min_value=0.0,
-    value=4.0
+    value=3.0
 )
-Age = st.number_input(
-    "Enter the age of the House",
+
+Floor = st.number_input(
+    "Enter the number of floors",
     min_value=0.0,
-    value=7.0
+    value=2.0
 )
+
 if st.button("Predict"):
-    if Area < 200:
-        st.error("Area cannot be less than 200.")
-    
-    elif Area > 600:
-        st.error("Area cannot be more than 600.")
+
+    if Area < 600:
+        st.error("Area cannot be less than 600.")
+
+    elif Area > 3000:
+        st.error("Area cannot be more than 3000.")
+
     elif Bedrooms < 1:
         st.error("Number of bedrooms must be at least 1.")
 
-    elif Bedrooms > 7:
-        st.error("Number of bedrooms cannot be more than 7.")
-    elif Age < 5:
-        st.error("House age cannot be less than 5 years.")
+    elif Bedrooms > 4:
+        st.error("Number of bedrooms cannot be more than 4.")
 
-    elif Age > 25:
-        st.error("House age cannot be more than 25 years.")
+    elif Floor < 1:
+        st.error("Number of floors must be at least 1.")
+
+    elif Floor > 10:
+        st.error("Number of floors cannot be more than 10.")
 
     else:
-        prediction = model.predict([[Area, Bedrooms, Age]])
+        prediction = model.predict([[Area, Bedrooms, Floor]])
 
         st.success(
-            f"Predicted House Price: ${prediction[0]:,.2f}"
+            f"Predicted House Price: ₹{prediction[0]:,.2f} Lakhs"
         )
