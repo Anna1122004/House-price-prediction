57f8134605c9d529e1451596456dd913899f4b88
Anna1122004 <mathewt9747592371@gmail.com>
2026-09-17 12:46:06 +0530

    Update House_price_prediction.py

--- House_price_prediction.py
+++ House_price_prediction.py
@@ -2,32 +2,41 @@ import streamlit as st
 import joblib
 model = joblib.load("house_price_prediction.pkl")
 st.title("House Price Prediction")
-Area= st.number_input(
+Area = st.number_input(
     "Enter Area",
-    min_value=200.0,
-    max_value=600.0,
+    min_value=0.0,
     value=400.0
 )
-Bedrooms=st.number_input(
+Bedrooms = st.number_input(
     "Enter number of bedrooms",
-    min_value=1.0,
-    max_value=7.0,
+    min_value=0.0,
     value=4.0
 )
-Age=st.number_input(
-  "Enter the age of the House",
-  min_value=5.0,
-  max_value=25.0,
-  value=7.0
+Age = st.number_input(
+    "Enter the age of the House",
+    min_value=0.0,
+    value=7.0
 )
-
-
 if st.button("Predict"):
-   
+    if Area < 200:
+        st.error("Area cannot be less than 200.")
     
-    if Area > 600:
+    elif Area > 600:
         st.error("Area cannot be more than 600.")
+    elif Bedrooms < 1:
+        st.error("Number of bedrooms must be at least 1.")
+
+    elif Bedrooms > 7:
+        st.error("Number of bedrooms cannot be more than 7.")
+    elif Age < 5:
+        st.error("House age cannot be less than 5 years.")
+
+    elif Age > 25:
+        st.error("House age cannot be more than 25 years.")
+
     else:
         prediction = model.predict([[Area, Bedrooms, Age]])
 
-        st.success(f"Predicted House Price: ${prediction[0]:,.2f}")
+        st.success(
+            f"Predicted House Price: ${prediction[0]:,.2f}"
+        )
